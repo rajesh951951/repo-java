b00c00c1159a79debde2c85140a947ddf0c59d56
rajesh951951 <151846791+rajesh951951@users.noreply.github.com>
2024-01-13 19:45:22 +0530

    Added insertInLinkedList

--- src/main/java/dataStructures/LinkedList/Singly/SinglyLinkedList.java
+++ src/main/java/dataStructures/LinkedList/Singly/SinglyLinkedList.java
@@ -15,4 +15,48 @@ public class SinglyLinkedList {
         size = 1;
         return head;
     }
+
+    public void insertInLinkedList(int nodeValue, int location){
+        Node node = new Node();
+        node.value = nodeValue;
+
+        if(head == null){ // Insert when LinkedList is empty
+            createSinglyLinkedList(nodeValue);
+        }
+        else if(location == 0) { // Inserting at the beginning of LinkedList
+            node.next = head;
+            head = node;
+        } else if(location >= size){ // Inserting at the end of LinkedList
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        } else{ // Inserting at the Specified location of SinglyLinkedList i.e. in-between LinkedList
+            Node refNode = head;
+            int searchIndex = 0;
+            while(searchIndex < location - 1){ //traverse till we get specified location
+                refNode = refNode.next;
+                searchIndex++;
+            }
+            Node nextNode = refNode.next;
+            refNode.next = node;
+            node.next = nextNode;
+        }
+        size++;
+    }
+
+    public void printLinkedList(Node node){
+        while (node != null && node.next != null) {
+            System.out.print(node.value + "->");
+            node = node.next;
+        }
+
+        if (node != null) {
+            System.out.println(node.value);
+        } else {
+            System.out.println("Empty LinkedList");
+        }
+    }
+
+
+
 }
